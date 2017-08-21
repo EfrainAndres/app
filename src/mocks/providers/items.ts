79adb8b8abda34067c96bb758febe5f221b6bd87
EfrainAndres <efrainvergara.udec@gmail.com>
2017-08-21 15:03:05 -0500

    Arreglo Icon

---
+++ src/mocks/providers/items.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+import { Item } from '../../models/item';
+
+@Injectable()
+export class Items {
+  items: Item[] = [];
+
+  defaultItem: any = {
+    "name": "APX 5000",
+    "profilePic": "assets/img/apx5000.png",
+    "about": "Este equipo ha sido probado y se ha corroborado que respeta los límites estipulados para dispositivos digitales de la Clase B, conforme a lo dispuesto por la Parte 15 de las Directivas de la FCC. Estos límites están diseñados para ofrecer una protección razonable contra interferencias nocivas en una instalación residual. Este equipo genera, utiliza y puede emitir energía de radiofrecuencia (RF) y si no se lo instala y utiliza según las instrucciones, puede causar interferencia nociva a las comunicaciones de radio. No obstante, no existe garantía de que no se produzca interferencia en una instalación específica.",
+  };
+
+
+  constructor(public http: Http) {
+    let items = [
+      {
+        "name": "APX 5000",
+        "profilePic": "assets/img/apx5000.jpg",
+        "about": "Este equipo ha sido probado y se ha corroborado que respeta los límites estipulados para dispositivos digitales de la Clase B, conforme a lo dispuesto por la Parte 15 de las Directivas de la FCC. Estos límites están diseñados para ofrecer una protección razonable contra interferencias nocivas en una instalación residual. Este equipo genera, utiliza y puede emitir energía de radiofrecuencia (RF) y si no se lo instala y utiliza según las instrucciones, puede causar interferencia nociva a las comunicaciones de radio. No obstante, no existe garantía de que no se produzca interferencia en una instalación específica.",
+      },
+      {
+        "name": "APX 7000",
+        "profilePic": "assets/img/apx7000.jpg",
+        "about": "Los radios móviles APX están entre los radios de dos vías más sofisticados que se encuentran disponibles. Tienen un nuevo diseño resistente para los usuarios que necesitan radios de alto rendimiento, calidad y confiabilidad en sus comunicaciones diarias. Esta nueva arquitectura proporciona una gran variedad de funciones avanzadas y heredadas, lo que da como resultado una solución de comunicaciones de radio de dos vías más rentable."
+      },
+      {
+        "name": "APX 7500",
+        "profilePic": "assets/img/apx7500.jpg",
+        "about": "Los radios móviles APX están entre los radios de dos vías más sofisticados que se encuentran disponibles. Tienen un nuevo diseño resistente para los usuarios que necesitan radios de alto rendimiento, calidad y confiabilidad en sus comunicaciones diarias. Esta nueva arquitectura proporciona una gran variedad de funciones avanzadas y heredadas, lo que da como resultado una solución de comunicaciones de radio de dos vías más rentable."
+      }
+    ];
+
+    for (let item of items) {
+      this.items.push(new Item(item));
+    }
+  }
+
+  query(params?: any) {
+    if (!params) {
+      return this.items;
+    }
+
+    return this.items.filter((item) => {
+      for (let key in params) {
+        let field = item[key];
+        if (typeof field == 'string' && field.toLowerCase().indexOf(params[key].toLowerCase()) >= 0) {
+          return item;
+        } else if (field == params[key]) {
+          return item;
+        }
+      }
+      return null;
+    });
+  }
+
+  add(item: Item) {
+    this.items.push(item);
+  }
+
+  delete(item: Item) {
+    this.items.splice(this.items.indexOf(item), 1);
+  }
+}
---
+++ src/pages/tutorial/tutorial.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { MenuController, NavController } from 'ionic-angular';
+
+import { WelcomePage } from '../welcome/welcome';
+
+import { TranslateService } from '@ngx-translate/core';
+
+
+
+export interface Slide {
+  title: string;
+  description: string;
+  image: string;
+}
+
+@Component({
+  selector: 'page-tutorial',
+  templateUrl: 'tutorial.html'
+})
+export class TutorialPage {
+  slides: Slide[];
+  showSkip = true;
+
+  constructor(public navCtrl: NavController, public menu: MenuController, translate: TranslateService) {
+    translate.get(["TUTORIAL_SLIDE1_TITLE",
+      "TUTORIAL_SLIDE1_DESCRIPTION",
+      "TUTORIAL_SLIDE2_TITLE",
+      "TUTORIAL_SLIDE2_DESCRIPTION",
+      "TUTORIAL_SLIDE3_TITLE",
+      "TUTORIAL_SLIDE3_DESCRIPTION",
+    ]).subscribe(
+      (values) => {
+        console.log('Loaded values', values);
+      });
+  }
+
+  startApp() {
+    this.navCtrl.setRoot(WelcomePage, {}, {
+      animate: true,
+      direction: 'forward'
+    });
+  }
+
+  onSlideChangeStart(slider) {
+    this.showSkip = !slider.isEnd();
+  }
+
+  ionViewDidEnter() {
+    // the root left menu should be disabled on the tutorial page
+    this.menu.enable(false);
+  }
+
+  ionViewWillLeave() {
+    // enable the root left menu when leaving the tutorial page
+    this.menu.enable(true);
+  }
+
+}
